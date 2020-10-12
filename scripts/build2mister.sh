fc84274e330ef38752e57199916d79573ef6966e
jorge.fuertes <jorge@jorgefuertes.com>
2020-10-12 19:56:36 +0200

    Better build

--- scripts/build2mister.sh
+++ scripts/build2mister.sh
@@ -1,8 +1,32 @@
 #!/usr/bin/env bash
 
+if [[ "$0" != *scripts/build2mister.sh ]]
+then
+	echo "Please, execute from project's root directory"
+	exit 1
+fi
+
+if [[ -f .mister_ip ]]
+then
+	MISTER_IP=$(cat .mister_ip)
+else
+	while [[ 1 ]]
+	do
+		echo -n "Your Mister's IP? "
+		read MISTER_IP
+		echo -n "Mister IP ${MISTER_IP}, that's correct? (y/n) "
+		read -n1 yn
+		echo
+		if [[ "$yn" == "y" ]]
+		then
+			echo $MISTER_IP > .mister_ip
+			break
+		fi
+	done
+fi
+
 EXE_NAME="mister-modemu"
 VER=$(git describe --tags --contains)
-MISTER="root@192.168.1.10"
 DEST="/media/fat"
 
 WHO=$(whoami)
@@ -39,7 +63,7 @@ then
     exit 1
 fi
 
-ssh $MISTER rm "${DEST}/mister-modemu*" &> /dev/null
-scp bin/$o "${MISTER}:${DEST}/mister-modemu.tmp"
-ssh $MISTER mv "${DEST}/mister-modemu.tmp" "${DEST}/${o}"
-ssh $MISTER "${DEST}/${o} -e dev"
+ssh root@$MISTER_IP rm "${DEST}/mister-modemu*" &> /dev/null
+scp bin/$o "root@${MISTER_IP}:${DEST}/mister-modemu.tmp"
+ssh root@$MISTER_IP mv "${DEST}/mister-modemu.tmp" "${DEST}/${o}"
+ssh root@$MISTER_IP "${DEST}/${o} -e dev"
