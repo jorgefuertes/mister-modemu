db841ae321a7566069270977041f14e1956478dd
jorge.fuertes <jorge@jorgefuertes.com>
2020-10-14 16:31:55 +0200

    Mister install script

--- scripts/mister_install.sh
+++ scripts/mister_install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 GHSCRIPTS="https://raw.githubusercontent.com/jorgefuertes/mister-modemu/master/scripts/"
+BINDIR="/media/fat/scripts"
+STARTSCRIPT="RetroWiki_Modemu_Start.sh"
+STOPSCRIPT="RetroWiki_Modemu_Stop.sh"
 
 check() {
 	if [[ $1 -eq 0 ]]
@@ -13,11 +16,11 @@ check() {
 }
 
 echo -n "Start script..."
-curl -kLs $GHSCRIPTS/RetroWiki_Modemu_Start.sh
+curl -kLs -o $BINDIR/$STARTSCRIPT $GHSCRIPTS/$STARTSCRIPT
 check($?)
 
 echo -n "Stop script..."
-curl -kLs $GHSCRIPTS/RetroWiki_Modemu_Stop.sh
+curl -kLs -o $BINDIR/$STOPSCRIPT $GHSCRIPTS/$STOPSCRIPT
 check($?)
 
 echo "Modemu installed, use Scripts menu to launch or stop it"
