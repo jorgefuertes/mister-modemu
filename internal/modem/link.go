2dd03897bc89c1132c87a525072f4a45f2c46467
jorge.fuertes <jorge@jorgefuertes.com>
2020-10-18 13:11:48 +0200

    UGophy support

--- internal/modem/link.go
+++ internal/modem/link.go
@@ -9,45 +9,46 @@ import (
 	"github.com/jorgefuertes/mister-modemu/internal/console"
 )
 
+func (m *Modem) prefix(id uint8) string {
+	return fmt.Sprintf("NET/LISTEN/%v/%v:%v", id, m.connections[id].ip, m.connections[id].port)
+}
+
 func (m *Modem) listenLink(id uint8) {
 	var err error
 	var res string
 	var b []byte = make([]byte, 2048)
 	var n int
 
-	prefix := fmt.Sprintf("NET/LISTEN/%v/%v:%v", id, m.connections[id].ip, m.connections[id].port)
-
-	console.Debug(prefix, "Listening")
+	console.Debug(m.prefix(id), "Listening")
 
 	for {
 		// Check if close on
 		if m.connections[id].close {
-			console.Debug(prefix, "Closing")
-			m.connections[id] = nil
+			m.close(id)
 			break
 		}
 		// Set timeout
 		err = m.connections[id].conn.SetReadDeadline(time.Now().Add(30 * time.Second))
 		if err != nil {
-			console.Warn(prefix, "SetReadDeadline failed: ", err)
-			console.Warn(prefix, "Closed")
+			console.Warn(m.prefix(id), "SetReadDeadline failed: ", err)
+			console.Warn(m.prefix(id), "Closed")
 			return
 		}
 		// Read
 		n, err = m.connections[id].conn.Read(b)
 		if err != nil {
 			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-				console.Debug(prefix, "TimeOut - resuming")
+				console.Debug(m.prefix(id), "TimeOut - resuming")
 				continue
-			} else {
-				console.Debug(prefix, err.Error())
-				break
 			}
+			console.Debug(m.prefix(id), err.Error())
+			m.close(id)
+			break
 		}
 		// Something received?
 		// (+CIPMUX=0)+IPD,<len>[,<remote IP>,<remote port>]:<data>
 		// (+CIPMUX=1)+IPD,<link ID>,<len>[,<remote IP>,<remote port>]:<data>
-		console.Debug(prefix, "Received ", n, " bytes")
+		console.Debug(m.prefix(id), "Received ", n, " bytes")
 		if n > 0 {
 			cut := b[0:n]
 			if cfg.IsDev() {
@@ -60,7 +61,7 @@ func (m *Modem) listenLink(id uint8) {
 					hex += fmt.Sprintf("%02X", cut[i])
 					str += byteToStr(cut[i])
 					if count == 20 || i == n-1 {
-						console.Debug(prefix, hex, "| ", str)
+						console.Debug(m.prefix(id), hex, "| ", str)
 						count = 0
 						hex = ""
 						str = ""
@@ -78,7 +79,15 @@ func (m *Modem) listenLink(id uint8) {
 			m.write(res + ":")
 			m.writeBytes(&cut)
 			m.write(cr, lf)
-			console.Debug(prefix, "EOT")
+			console.Debug(m.prefix(id), "Internal EOD")
 		}
 	}
+}
+
+// close and remove connection
+func (m *Modem) close(id uint8) {
+	console.Debug(m.prefix(id), "Link closing")
+	m.writeLn(fmt.Sprintf("%v,CLOSED", id))
+	console.Debug(m.prefix(id), "Removing connection")
+	m.connections[id] = nil
 }
